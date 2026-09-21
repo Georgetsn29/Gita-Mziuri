cd941f548599b5617c77456db80c8554d565c618
Georgetsn29 <giotsnobiladze5@gmail.com>
2026-09-21 18:02:05 +0400

    homework while and for loops

--- javascript/loops/for-loops/script.js
+++ javascript/loops/for-loops/script.js
@@ -37,9 +37,12 @@
     
 // }
 
+
+
+
 // ! მასივში ობიექტები - 18< > კლუბში არ ვუშვებთ
 
-// const students = [
+// const studentIsAdult = [
 //     {
 //         name: 'გიორგი',
 //         age: 23
@@ -54,10 +57,10 @@
 //     }
 // ]
 
-// for(let i = 0; i < students.length; i++) {
+// for(let i = 0; i < studentIsAdult.length; i++) {
 
-//     let age_line = students[i].age >= 18 ? 'შეუძლია კლუბში შესვლა' : 'კლუბში ვერ შევა'
-//     console.log(`${students[i].name} არის ${students[i].age} წლის: ${age_line}.`);
+//     let age_line = studentIsAdult[i].age >= 18 ? 'შეუძლია კლუბში შესვლა' : 'კლუბში ვერ შევა'
+//     console.log(`${studentIsAdult[i].name} არის ${studentIsAdult[i].age} წლის: ${age_line}.`);
     
 // }
 
@@ -67,44 +70,88 @@
 // ! დათვალეთ რამდენმა სტუდენტმა ვერ ჩააბარა ან ჩააბარა გამოცდა;
 // ! იპოვეთ რამდენმა მიიღო 85-ზე მაღალი ქულა. 
 
-let students = [
-    {
-        name: 'ანა',
-        grade: [45, 90, 18]
-    },
-    {
-        name: 'გიორგი',
-        grade: [10, 98, 38]
-    },
-    {
-        name: 'ნინი',
-        grade: [89, 92, 78]
-    }
-]
+// let students = [
+//     {
+//         name: 'ანა',
+//         grade: [45, 90, 18]
+//     },
+//     {
+//         name: 'გიორგი',
+//         grade: [10, 98, 38]
+//     },
+//     {
+//         name: 'ნინი',
+//         grade: [89, 92, 78]
+//     }
+// ]
 
-for(let i = 0; i < students.length; i++) {
+// for(let i = 0; i < students.length; i++) {
 
-    let total = 0
+//     let total = 0
 
-    for(let g = 0; g < students[i].grade.length; g++) {
-        total += students[i].grade[g];
-    }
+//     for(let g = 0; g < students[i].grade.length; g++) {
+//         total += students[i].grade[g];
+//     }
 
-    let avg = total / students[i].grade.length
+//     let avg = total / students[i].grade.length
 
-    let pass = avg >= 51 ? 'ჩააბარა' :
-                           'ჩაიჭრა'
+//     let pass = avg >= 51 ? 'ჩააბარა' :
+//                            'ჩაიჭრა'
     
-    console.log(`${students[i].name} საშვალო ქულაა ${avg}. ${pass}`);
+//     console.log(`${students[i].name} საშვალო ქულაა ${avg}. ${pass}`);
  
-}
+// }
 
 
 
 
-// ! იპოვეთ 100-დან უმაღლესი რიცხვი  და უმდაბლესი. 
-// ! იპოვეთ ყველა ლუწი რიცხვი მასივში და დააჯამეთ. 
+// ! იპოვეთ 100-დან უმაღლესი რიცხვი  და უმდაბლესი.
 // ! გამოიყენეთ min და max საჭიროების შემთხვევაში. 
+
+// let numbers = [20, 40, 21, 121, 440, 23]
+
+// let minNum = Math.min(...numbers)
+// let maxNum = Math.max(...numbers)
+
+// console.log(`ამ რიცხვებიდან: ${numbers} 
+// მაქსიმალურია - ${minNum}
+// მინიმალურია - ${maxNum}`);
+    
+
+// ! იპოვეთ ყველა ლუწი რიცხვი მასივში და დააჯამეთ. 
+
+
+// * ლუწის რიცხვების დაჯამება 
+// let evenSum = 0
+
+// for(i = 0; i <= 100; i++) {
+
+//     if(i % 2 === 0){
+//         evenSum += i
+//     }
+
+// }
+
+// console.log(`ლუწი რიცხვების ჯამია: ${evenSum}`);
+
+// * ლუწი რიცხვების დაჯამება 2
+// let num = [1, 2, 3, 4, 5, 6, 76, 21, 54, 100]
+// let sum = 0
+
+// for (let i = 0; i < num.length; i++) {
+//     if(num[i] % 2 === 0) {
+//         sum += num[i]
+        
+//         console.log(num[i]);
+        
+//     }
+
+    
+// }
+
+// console.log(`ლუწი რიცხვებია ჯამია: ${sum}`);
+
+
 
 
 // ! თუ რიცხვები მეორდება, მხოლოდ ერთხელ შემოიტანეთ, თუ არ მეორდება ცალკე დააჯამათ
@@ -114,4 +161,26 @@ for(let i = 0; i < students.length; i++) {
 
 
 
-                
+
+
+
+// ! for of - გამოიყენება მასივის მნიშვნელობასთან
+// ! for in - გამოიყენება მხოლოდ ობიექტის key-თან
+
+// const fruits = ['apple', 'banana', 'orange']
+
+// for(const fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// const person = {
+//     name: 'john',
+//     surname: 'Doe',
+//     age: 309,
+//     city: 'Tbilisi'
+// }
+
+// for (const user in person) {
+//     console.log(user, person[user]);
+    
+// }
---
+++ javascript/loops/while-loops/script.js
@@ -0,0 +1,83 @@
+// let i = 0
+
+// while (i < 5) {
+//     console.log(i, 'Hellooo');
+//     i++
+// }
+
+// const colors = ['blue', 'yellow', 'black', 'red', 'green']
+// let j = 0
+// while (j < colors.length) {
+//     console.log(j, colors[j]);
+//     j++
+// }
+
+
+
+
+// let n = 0
+// let sum = 0
+// while (n <= 15) {
+//     sum += n
+
+//     n++
+// }   
+//     console.log(sum);
+
+
+
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let i = 0
+// let even = 0
+
+// while (i < num.length) {
+
+//     if(num[i] % 2 === 0) {
+//         even += num[i]
+//     }
+    
+//     i++
+// }
+
+// console.log(even);
+
+
+
+// ! მაქვს სტრინგი hello world!!, ეს სიტყვა უნდა დამიმარცვლოს ლუპმა
+// ! hello world!! - შემოატრიალეთ და ისე დალუპეთ სახელი
+// ! ხილის მასივი, 5-6 ასო-ბგერაზე მეტი მხოლოდ ის დაიპრტინტოს. 
+// ! მასივიდან იპოვეთ ყველაზე გრძელი სიტყვა და დაბეჭდეთ.
+
+
+// let word = 'hello world!!'
+
+// // * დამარცვლა
+// let result = word.split("")
+// console.log(result);
+
+// // * შეტრიალებული დამარცვლა
+// let reversed = word.split("").reverse()
+// console.log(reversed);
+
+
+
+// let fruits = ['ვაშლი', 'მსხალი', 'ატამი', 'ბანანი', 'ანანასი', 'ატამი']
+
+// for (let i = 0; i < fruits.length; i++) {
+
+//     if (fruits[i].length >= 6) {
+
+//         console.log(fruits[i]);
+        
+//     }
+// }
+
+// let longest = fruits[0]
+
+// for (let i = 1; i < fruits.length; i++) {
+//     if (fruits[i].length > longest.length) {
+//         longest = fruits[i]
+//     }
+// }
+
+// console.log(`გრძელი სიტყვაა ${longest}`);
